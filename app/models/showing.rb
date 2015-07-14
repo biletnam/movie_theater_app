7e42fdc1dc3f912549df14ffbc478996903f45f4
Paul Gonzalez <pjg5005@gmail.com>
2015-07-14 15:28:26 -0500

    Builds initial UI flow for checkout

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -2,5 +2,17 @@ class Showing < ActiveRecord::Base
   default_scope { order('time_slot ASC') }
 
   belongs_to :screening_room
-  has_one :movie
+
+  def amount_of_seats_remaining
+    self.screening_room.seat_quantity - self.tickets_sold
+  end
+
+  def tickets_sold
+    0
+  end
+
+  def movie
+    Movie.find(self.movie_id)
+  end
+
 end
